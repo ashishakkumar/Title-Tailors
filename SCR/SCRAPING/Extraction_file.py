8cc8d2be6e8787075a7df652bca402ac9de72a8e
Karthik Mahesh Rathod <karthikmrathod1999@gmail.com>
2024-01-20 21:38:47 +0530

    updated the gcp condition

--- SCR/SCRAPING/Extraction_file.py
+++ SCR/SCRAPING/Extraction_file.py
@@ -1,10 +1,11 @@
-from py_functions_data import  fetch_and_update_dataframe, upload_to_storage, upload_directory_to_gcp
+from py_functions_data import  fetch_and_update_dataframe, upload_directory_to_gcp
 import os
 import warnings
 warnings.filterwarnings("ignore")
 import dotenv 
 dotenv.load_dotenv()
 
+# the function fetch_and_update_dataframe is defined in the file py_functions_data.py, it creates a dataframe from the ADS API
 fetch_and_update_dataframe(
     api_token=os.getenv('API_TOKEN'),
     query=os.getenv('QUERY'), 
@@ -18,11 +19,16 @@ fetch_and_update_dataframe(
     percentile_value=float(os.getenv('PERCENTILE_VALUE')), 
     instruction_text=os.getenv('INSTRUCTION_TEXT')
 )
+# The code block is checking the value of the environment variable `upload_to_gcp`. If the value is
+# not empty or False, it proceeds to upload a directory to Google Cloud Storage (GCP).
+upload_to_gcp = os.getenv('upload_to_gcp')
+if upload_to_gcp == True:
+    # 2. Set JSON key as environment variable
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=os.getenv("key_path")
 
-# 2. Set JSON key as environment variable
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=os.getenv("key_path")
+    # # 3. Specify a bucket name and other details
+    bucket_name = os.getenv("bucket_name")
 
-# # 3. Specify a bucket name and other details
-bucket_name = os.getenv("bucket_name")
-
-upload_directory_to_gcp(bucket_name =bucket_name , source_directory =f"/data/output", destination_blob_prefix = "new/data/")
+    upload_directory_to_gcp(bucket_name =bucket_name , source_directory =f"/data/output", destination_blob_prefix = "new/data/")
+else:
+    print("upload_to_gcp is not set to True, skipping upload to GCP")
--- SCR/SCRAPING/py_functions_data.py
+++ SCR/SCRAPING/py_functions_data.py
@@ -8,6 +8,31 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def fetch_data(api_token, query, start=0, rows=2000, params=None, sort=None, subset=None, **kwargs):
+    """
+    The `fetch_data` function fetches data from an API using the provided parameters and returns the
+    extracted relevant information.
+    
+    :param api_token: The `api_token` parameter is the access token required to authenticate and
+    authorize the API request. It is used to identify and validate the user making the request
+    :param query: The query parameter is used to specify the search query for the API. It is a string
+    that represents the search query you want to perform
+    :param start: The `start` parameter specifies the starting index of the results to be fetched. It is
+    used for pagination, allowing you to retrieve results in batches, defaults to 0 (optional)
+    :param rows: The "rows" parameter specifies the maximum number of results to be returned by the API.
+    In this case, it is set to 2000, meaning that the API will return a maximum of 2000 documents that
+    match the query, defaults to 2000 (optional)
+    :param params: The `params` parameter is used to specify the fields that you want to retrieve from
+    the API response. It should be a comma-separated string of field names. For example, if you want to
+    retrieve the title and author fields, you can pass `params="title,author"`
+    :param sort: The "sort" parameter is used to specify the sorting order of the results. It can be set
+    to a field name to sort the results based on that field in ascending order. To sort in descending
+    order, you can prefix the field name with a minus sign (-). For example, if you want
+    :param subset: The "subset" parameter is used to filter the results based on a specific subset of
+    the data. It is typically used to narrow down the search to a specific category or field. For
+    example, if you are searching for articles related to astronomy, you can use the "subset" parameter
+    to filter the
+    :return: The function `fetch_data` returns the documents retrieved from the API query.
+    """
     # API query
     encoded_query = urlencode({
         "q": query,
@@ -28,6 +53,45 @@ def fetch_data(api_token, query, start=0, rows=2000, params=None, sort=None, sub
     return docs
 
 def fetch_and_update_dataframe(api_token, query, max_rows, start, params=None, sort=None, subset=None, percentile_filtering=False, percentile_metric='read_count', percentile_value=0.75, instruction_text=""):
+    """
+    The function fetches data from an API, updates a DataFrame, adds a new column with a link, saves the
+    DataFrame to a CSV file, performs filtering and cleaning operations on the DataFrame, and returns
+    the final DataFrame.
+    
+    :param api_token: The `api_token` parameter is the API token required to access the data from the
+    API. You need to provide a valid API token to authenticate and fetch the data
+    :param query: The query parameter is used to specify the search query for fetching data from the
+    API. It is a string that defines the search criteria, such as keywords, authors, or specific fields
+    to search within the data
+    :param max_rows: The `max_rows` parameter specifies the maximum number of rows to fetch and update
+    in the dataframe
+    :param start: The `start` parameter is used to specify the starting index for fetching data from the
+    API. It determines the position from which the data retrieval should begin
+    :param params: The `params` parameter is used to pass additional parameters to the API call. It is a
+    dictionary where the keys are the parameter names and the values are the parameter values. These
+    parameters can be used to filter or modify the data that is fetched from the API
+    :param sort: The "sort" parameter is used to specify the sorting order of the fetched data. It can
+    be set to "date" to sort the data by date, "relevance" to sort the data by relevance, or
+    "citation_count" to sort the data by citation count
+    :param subset: The "subset" parameter is used to specify a subset of the data to fetch. It can be a
+    list of field names to include in the fetched data. For example, if you only want to fetch the
+    "title" and "abstract" fields, you can pass subset=['title', 'abstract
+    :param percentile_filtering: The parameter "percentile_filtering" is a boolean value that determines
+    whether or not to filter the DataFrame based on a percentile threshold. If set to True, the
+    DataFrame will be filtered to keep only the rows with a specified metric (defined by
+    "percentile_metric") above the threshold (defined by, defaults to False (optional)
+    :param percentile_metric: The `percentile_metric` parameter is used to specify the column in the
+    DataFrame that will be used for percentile filtering. This column should contain numerical values,
+    defaults to read_count (optional)
+    :param percentile_value: The `percentile_value` parameter is a value between 0 and 1 that determines
+    the threshold for percentile filtering. It is used to filter the DataFrame to keep rows with a
+    specified metric above the threshold. For example, if `percentile_value` is set to 0.75, it
+    :param instruction_text: The `instruction_text` parameter is a string that represents the
+    instruction or label for the data. It is used to add a new column called "Instruction" to the
+    DataFrame. This column will contain the same instruction text for all rows in the DataFrame
+    :return: the final filtered and processed DataFrame.
+    """
+   
     chunk_size = 2000
     dataframe = pd.DataFrame()
 
@@ -82,18 +146,21 @@ def fetch_and_update_dataframe(api_token, query, max_rows, start, params=None, s
     return 
 
 
-# 4. Define a special function
-def upload_to_storage(bucket_name: str, source_file_path: str, destination_blob_path: str):
-  """Uploads a file to the bucket."""
-  storage_client = storage.Client()
-  bucket = storage_client.get_bucket(bucket_name)
-  blob = bucket.blob(destination_blob_path)
-  blob.upload_from_filename(source_file_path)
-  print(f'The file {source_file_path} is uploaded to GCP bucket path: {destination_blob_path}')
-  return None
+
 
 
 def upload_directory_to_gcp(bucket_name, source_directory, destination_blob_prefix):
+    """
+    The function `upload_directory_to_gcp` uploads a directory to a Google Cloud Storage bucket.
+    
+    :param bucket_name: The name of the GCP bucket where you want to upload the directory
+    :param source_directory: The source_directory parameter is the path to the directory on your local
+    machine that you want to upload to the GCP bucket
+    :param destination_blob_prefix: The `destination_blob_prefix` parameter is a string that represents
+    the prefix to be added to the destination blob name. It is used to specify the directory structure
+    within the GCP bucket where the files will be uploaded. For example, if the
+    `destination_blob_prefix` is set to "data/files",
+    """
     """Uploads a directory to the GCP bucket."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
